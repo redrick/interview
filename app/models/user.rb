9664523fbad9545467bb5830947404fd946ac8ad
Martin Sojka <sojka.martin@gmail.com>
2015-09-03 15:29:50 +0200

    Validate presence of nemae, surname and email of the User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+  # Scopes
+
+  # Associations
+
+  # Validations
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :email, presence: true
+
+  # Callbacks
+
+
 end
