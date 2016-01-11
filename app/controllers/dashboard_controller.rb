494a24c4373855ffb5f2bd97cc64f859c28daff2
Tom Meinlschmidt <tomas@meinlschmidt.com>
2016-01-11 22:24:25 +0100

    more tasks

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < AuthenticatedController
   def index
+    @unfinished_tasks = current_user.tasks.unfinished
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Interview::Application.routes.draw do
   devise_for :admins
   devise_for :managers
 
-  resources :tasks, except: [:show]
+  resources :tasks, except: [:show] do
+    get :toggle, on: :member
+  end
 
   root 'dashboard#index'
 
