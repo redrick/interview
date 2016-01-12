867060db0b7e2dcf9f5ec12634891bcc062f9386
Tom Meinlschmidt <tomas@meinlschmidt.com>
2016-01-12 03:15:04 +0100

    email should be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
   # validations
   # not 100% regexp
   validates :email, presence: true, format: { with: /\A[a-z0-9\.-_]+@[a-z0-9]+(-?[a-zA-Z0-9])*(\.[a-z](-?[a-z0-9])*)+\Z/ }
+  validates :email, uniqueness: true
   validates :phone, format: { with: /\A\d{6,}\Z/ }, allow_blank: true
 
   validates :name, length: {minimum: 2, maximum: 32}, allow_blank: true
